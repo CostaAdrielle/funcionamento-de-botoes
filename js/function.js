1d1fb38b1515b561ff594b6d86954b55be5352ed
Adrielle Costa <adrielle.gasparc@gmail.com>
2018-08-22 00:18:24 -0300

    Testando, comparando e aprendendo a atribuir varias funcionalidades em botões

---
+++ js/function.js
@@ -0,0 +1,8 @@
+function botao(){
+  var botao1 = document.querySelector("botao")
+  alert("funciona");
+}
+function botao2(){
+  var botao2 = document.querySelector("botao2")
+  alert("não funciona!")
+}
